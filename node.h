8108381c898b660d6a957b62664b1e885ed2faf3
[Cole Brooks] <[colespencerbrooks@gmail.com]>
2019-03-12 14:16:07 -0600

    First node struct iteration

---
+++ node.h
@@ -0,0 +1,21 @@
+//**************************************
+// Group 8
+// Binary Tree Implementation
+// Node Struct
+// CSCI 205
+// Last Edit: 14:14 03/12/19
+//**************************************
+
+
+
+
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct node_struct {
+    int number;
+    struct node_struct* left;
+    struct node_struct* right;
+} Node;
+
+#endif
