8df5a7a947758e65ec3971e8250148e49753d1ca
[Cole Brooks] <[colespencerbrooks@gmail.com]>
2019-03-13 11:37:02 -0600

    Added header comments

--- node.c
+++ node.c
@@ -1,1 +1,9 @@
+//**************************************
+// Group 8
+// Binary Tree Implementation
+// Node Functions
+// CSCI 205
+// Last Edit: 11:36 03/13/19
+//**************************************
+
 #include "node.h"
