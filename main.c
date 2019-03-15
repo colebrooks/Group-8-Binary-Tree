4066b5374ffc3705803d6f70cc7ea010b2715638
[Cole Brooks] <[colespencerbrooks@gmail.com]>
2019-03-15 12:35:29 -0600

    Added cmd line args, file reading, & error codes.

--- main.c
+++ main.c
@@ -3,34 +3,56 @@
 // Binary Tree Implementation
 // Main Function
 // CSCI 205
-// Last Edit: 11:33 03/13/19
+// Last Edit: 12:34 03/15/19
 //**************************************
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "binarytree.h"
 
-int main() {
-   binary_tree bt;
-   bool found = false;
+int main(int argc, char* argv[]) {
+    FILE* inFile = NULL;
+    binary_tree bt;
+    bool found = false;
 
-   printf("Print in order\n");
-   printinorder(&bt);
-   printf("\nPrint pre order\n");
-   printpreorder(&bt);
-   printf("\nPrint post order\n");
-   printpostorder(&bt);
-   printf("\n");
-   found = search(&bt,33);
-   if (found){
-      printf("FOUND 33\n");
-   }
-   found = search(&bt,38);
-   if (found){
-      printf("FOUND 38\n");
-   }
-   printf("Number of elements in tree: %d\n", btsize(&bt));
-   printf("Tree height: %d\n", treeheight(&bt));
+    if(argc != 2) {
+        printf("Error: Invalid number of command line arguments.\n");
+        return 1;
+    }
+
+    initialize(&bt);
+
+    inFile = fopen(argv[1], "r");
+
+    if(inFile == NULL) {
+        printf("Error: File not opened.\n");
+        return 1;
+    }
+
+    while(!feof(inFile)) {
+        int item;
+        fscanf(inFile, "%d", &item);
+        insert(&bt, item);
+    }
+    fclose(inFile);
+
+    printf("Print in order\n");
+    printinorder(&bt);
+    printf("\nPrint pre order\n");
+    printpreorder(&bt);
+    printf("\nPrint post order\n");
+    printpostorder(&bt);
+    printf("\n");
+    found = search(&bt,33);
+    if (found){
+       printf("FOUND 33\n");
+    }
+    found = search(&bt,38);
+    if (found){
+       printf("FOUND 38\n");
+    }
+    printf("Number of elements in tree: %d\n", btsize(&bt));
+    printf("Tree height: %d\n", treeheight(&bt));
 }
 
 /*Program should exit with code 1 and print an error message if the number of command line arguments is wrong.
