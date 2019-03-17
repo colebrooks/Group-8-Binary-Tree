b41cc60d208218f6d345cb54d9119644fa626aee
Brendan <brendanjhagan@gmail.com>
2019-03-16 20:08:23 -0600

    Commented the myscript.sh code

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #this script will test for proper output when errors occur
 ./bst.exe myfile.txt
-
+# tests first text file
 ./bst.exe myfile2.txt
-
+#tests second text file
 ./bst.exe myfile3.txt
-
+#tests third text file
 ./bst.exe
-
+#tests for error code with invalid number of cmd line args
 ./bst.exe foo.txt
+#tests for error code invalid input file
